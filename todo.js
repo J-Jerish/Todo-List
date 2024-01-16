80e182dc6b6e0a456a335e5b70dc4de1115d90ab
Jerish J <jerish1112@gmail.com>
2024-01-16 15:43:03 +0530

    Last Commit

--- todo.js
+++ todo.js
@@ -14,6 +14,30 @@ function addTask(){
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        saveData()
     }
     inputBox.value = "";
 }
+
+//Cross icon funtionality -- To remove listed items
+
+listContainer.addEventListener("click" , (e) => {
+        if (e.target.tagName === "LI") {
+            e.target.classList.toggle("checked");
+            saveData()
+        }
+        else if (e.target.tagName === "SPAN") {
+            e.target.parentElement.remove();
+            saveData()
+        }
+    },false);
+
+//To save the date in the browser
+
+function saveData(){
+    localStorage.setItem("to-do data",listContainer.innerHTML);
+}
+function getData(){
+    listContainer.innerHTML=localStorage.getItem("to-do data");
+}
+getData(); //To show the stored data after refreshing 
